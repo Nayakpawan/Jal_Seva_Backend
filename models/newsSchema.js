a779a8ef839754e41d932516383bbc78f65cda08
Pavan kumar Nayak <110595483+Nayakpawan@users.noreply.github.com>
2026-06-15 22:39:46 +0530

    Update newsSchema.js

--- models/newsSchema.js
+++ models/newsSchema.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const noticeSchema = new mongoose.Schema({
 title: { type: String, required: true },
   description: { type: String, required: true },
+  link: String,
   image: { type: String }, 
   createdAt: { type: Date, default: Date.now }
 });
